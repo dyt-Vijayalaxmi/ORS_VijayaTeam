4cd8a9c7754d8f2fc586623dd63790b4c24cda36
dyt-Vijayalaxmi <67150537+dyt-Vijayalaxmi@users.noreply.github.com>
2020-06-30 11:19:11 +0530

    frist commit

---
+++ src/main/java/dyt_vl/ORS_VijayaTeam/App.java
@@ -0,0 +1,13 @@
+package dyt_vl.ORS_VijayaTeam;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
